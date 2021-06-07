7660d9c7a43f50b3868f8beb834d42bd60099834
bkrisna <b.krisnamurti@gmail.com>
2021-06-07 17:57:32 +0000

    recreate hero

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,19 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
 console.log('Hello Coders! :)');
+
+
+const hamburgerButtonElement = document.querySelector("#menu-btn");
+const drawerElement = document.querySelector("#mobile-menu");
+const mainElement = document.querySelector("main");
+ 
+ 
+hamburgerButtonElement.addEventListener("click", event => {
+    drawerElement.classList.toggle("open");
+    event.stopPropagation();
+});
+
+mainElement.addEventListener("click", event => {
+    drawerElement.classList.remove("open");
+    event.stopPropagation();
+})
