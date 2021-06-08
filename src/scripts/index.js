cfd8523204aac79ada922d9a518172f7fdf508f6
bkrisna <b.krisnamurti@gmail.com>
2021-06-08 07:44:23 +0000

    imporved navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,17 +4,20 @@ import '../styles/main.css';
 console.log('Hello Coders! :)');
 
 
-const hamburgerButtonElement = document.querySelector("#menu-btn");
-const drawerElement = document.querySelector("#mobile-menu");
+const hamburgerButtonElement = document.querySelector("#mobile-menu");
+const drawerElement = document.querySelector(".nav-list");
 const mainElement = document.querySelector("main");
  
  
 hamburgerButtonElement.addEventListener("click", event => {
-    drawerElement.classList.toggle("open");
+    drawerElement.classList.toggle("mobile-nav");
+    hamburgerButtonElement.classList.toggle("is-active");
     event.stopPropagation();
 });
 
 mainElement.addEventListener("click", event => {
     drawerElement.classList.remove("open");
+    drawerElement.classList.remove("mobile-nav");
+    hamburgerButtonElement.classList.remove("is-active");
     event.stopPropagation();
 })
